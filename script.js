9754db377c6d82c42851c3c56b3d5007a6c1ca7e
Madhan <madhan038s@gmail.com>
2024-02-06 23:23:12 +0530

    js updated

--- script.js
+++ script.js
@@ -224,5 +224,15 @@ const sendMail = function () {
     });
 };
 
+var viewMode = getCookie('view-mode');
+if (viewMode == 'desktop') {
+  viewport.setAttribute('content', 'width=1024');
+} else if (viewMode == 'mobile') {
+  viewport.setAttribute(
+    'content',
+    'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no'
+  );
+}
+
 // const submit = document.querySelector('.submit');
 // submit.addEventListener('click', sendMail);
